d94ee23dc57b0049ce3f8a9dc0a697170026c260
Kaire Viana <kaireviana20@gmail.com>
2025-07-06 14:48:49 -0300

    Atualiza codígo da loja com validações e melhorias.0

--- Python/ex70.py
+++ Python/ex70.py
@@ -2,20 +2,17 @@ soma = cont_num_mil = 0
 menor = None
 nome_menor = ''
 
-print('-'*20)
+print('-'*30)
 print('LOJA SUPER BARATÃO!')
-print('-'*20)
+print('-'*30)
 
 while  True:
-
-    while True:
-        nome = str(input('Nome do produto: '))
-        break
+    nome = str(input('Nome do produto: ')).strip()
 
     while True:   
         try: 
             preco = int(input('Preço: R$'))
-            soma += preco
+            soma +=(preco)
             if preco > 1000:
                 cont_num_mil += 1
             break
@@ -34,6 +31,9 @@ while  True:
         nome_menor = nome
 
     if continua == 'N':
-        print(f'O total de gastos foi de R${soma}')
+        print('='*30)
+        print(f'O total de gastos foi de R${soma:.2f}')
         print(f'{cont_num_mil} custam mais de R$1000')
-        print(f'{nome_menor} é o produto com menor valor')
+        print(f'{nome_menor} é o produto com menor valor (R${menor:.2f})')
+        print('='*30)
+        break
